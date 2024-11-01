c19c402102fcbfbf0c9cda48e0e8f249dabb8e94
Methal Abeywickrama <methal.abeywickrama@icloud.com>
2024-11-01 12:22:57 +0530

    Format with prettier

--- script.js
+++ script.js
@@ -1,70 +1,76 @@
-
-const gameboard = document.querySelectorAll('.square')
-let turn = 'O'
+const gameboard = document.querySelectorAll(".square");
+let turn = "O";
 
 const changeTurn = (turn) => {
-  if (turn == 'O') {
-    return 'X'
+  if (turn == "O") {
+    return "X";
   } else {
-    return 'O'
+    return "O";
   }
-}
+};
 
 const checkWin = () => {
-  console.log('somethins')
-  const winConditions = [['1', '2', '3'], ['4', '5', '6'], ['7', '8', '9'], 
-                         ['1', '4', '7'], ['2', '5', '8'], ['3', '6', '9'],
-                         ['1', '5', '9'], ['3', '5', '7']]
-  const OSquares = []
-  const XSquares = []
-  gameboard.forEach(square => {
+  console.log("somethins");
+  const winConditions = [
+    ["1", "2", "3"],
+    ["4", "5", "6"],
+    ["7", "8", "9"],
+    ["1", "4", "7"],
+    ["2", "5", "8"],
+    ["3", "6", "9"],
+    ["1", "5", "9"],
+    ["3", "5", "7"],
+  ];
+  const OSquares = [];
+  const XSquares = [];
+  gameboard.forEach((square) => {
     if (square.textContent == "O") {
-      OSquares.push(square.id)
+      OSquares.push(square.id);
     } else if (square.textContent == "X") {
-      XSquares.push(square.id)
+      XSquares.push(square.id);
     }
-  }) 
+  });
   const isWinner = (squares) => {
-    const win = winConditions.some(condition =>
-      condition.every(id => squares.includes(id))
+    const win = winConditions.some((condition) =>
+      condition.every((id) => squares.includes(id))
     );
-    const winCondition = winConditions.find(condition =>
-      condition.every(id => squares.includes(id))
+    const winCondition = winConditions.find((condition) =>
+      condition.every((id) => squares.includes(id))
     );
-    return [win, winCondition]
+    return [win, winCondition];
   };
 
   const deactivateBoard = () => {
-    document.getElementById('gameboard').style.pointerEvents = "none"
-  }
+    document.getElementById("gameboard").style.pointerEvents = "none";
+  };
 
   const colourWin = (condition) => {
-    condition.forEach(id => {
-      const square = document.getElementById(id)
-      square.classList.add('greensquare')
-    })
-  }
+    condition.forEach((id) => {
+      const square = document.getElementById(id);
+      square.classList.add("greensquare");
+    });
+  };
   if (isWinner(OSquares)[0]) {
-    deactivateBoard()
-    colourWin(isWinner(OSquares)[1])
+    deactivateBoard();
+    colourWin(isWinner(OSquares)[1]);
     console.log("O wins!");
   } else if (isWinner(XSquares)[0]) {
-    colourWin(isWinner(XSquares)[1])
-    deactivateBoard()
+    colourWin(isWinner(XSquares)[1]);
+    deactivateBoard();
     console.log("X wins!");
   }
-
-  
-}
+};
 const handleClick = (event) => {
-  square = event.target
-  if (square.textContent == 'X' || square.textContent == 'O' ) { return }
-  
-  square.textContent = turn
-  turn = changeTurn(turn)
-  checkWin()
-}
+  square = event.target;
+  if (square.textContent == "X" || square.textContent == "O") {
+    return;
+  }
+
+  square.textContent = turn;
+  turn = changeTurn(turn);
+  checkWin();
+};
 
 gameboard.forEach((square) => {
-  square.addEventListener('click', handleClick)
-})
+  square.addEventListener("click", handleClick);
+});
